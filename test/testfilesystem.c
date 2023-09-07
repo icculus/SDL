03a4bd114117f4a1245c7ec90e34905000fe50e3
Ryan C. Gordon <icculus@icculus.org>
2023-09-07 16:08:59 -0400

    Added debug logging to testfilesystem to see what happens on GitHub Actions.

--- test/testfilesystem.c
+++ test/testfilesystem.c
@@ -21,26 +21,35 @@ int main(int argc, char *argv[])
     char *base_path;
     char *pref_path;
 
+Uint64 prevticks = 0;
+#define STOPWATCH(what) { const Uint64 now = SDL_GetTicks(); SDL_Log("STOPWATCH: Step '%s' took %u ticks", what, (unsigned int) (now - prevticks)); prevticks = now; }
+STOPWATCH("Startup");
+
     /* Initialize test framework */
     state = SDLTest_CommonCreateState(argv, 0);
+STOPWATCH("SDLTest_CommonCreateState");
     if (state == NULL) {
         return 1;
     }
 
     /* Enable standard application logging */
     SDL_LogSetPriority(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO);
+STOPWATCH("SDL_LogSetPriority");
 
     /* Parse commandline */
     if (!SDLTest_CommonDefaultArgs(state, argc, argv)) {
         return 1;
     }
+STOPWATCH("SDLTest_CommonDefaultArgs");
 
     if (SDL_Init(0) == -1) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "SDL_Init() failed: %s\n", SDL_GetError());
         return 1;
     }
+STOPWATCH("SDL_Init");
 
     base_path = SDL_GetBasePath();
+STOPWATCH("SDL_GetBasePath");
     if (base_path == NULL) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't find base path: %s\n",
                      SDL_GetError());
@@ -50,6 +59,7 @@ int main(int argc, char *argv[])
     }
 
     pref_path = SDL_GetPrefPath("libsdl", "test_filesystem");
+STOPWATCH("SDL_GetPrefPath(\"libsdl\")");
     if (pref_path == NULL) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't find pref path: %s\n",
                      SDL_GetError());
@@ -59,6 +69,7 @@ int main(int argc, char *argv[])
     }
 
     pref_path = SDL_GetPrefPath(NULL, "test_filesystem");
+STOPWATCH("SDL_GetPrefPath(NULL)");
     if (pref_path == NULL) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't find pref path without organization: %s\n",
                      SDL_GetError());
