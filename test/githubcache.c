0ab3f1624861be9767088411eba2f0fc4aad2326
Ryan C. Gordon <icculus@icculus.org>
2023-09-07 19:40:02 -0400

    Try to force 32-bit Shell32.dll to load before the test.

---
+++ test/githubcache.c
@@ -0,0 +1,22 @@
+/*
+  Copyright (C) 1997-2023 Sam Lantinga <slouken@libsdl.org>
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely.
+*/
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+int main(int argc, char *argv[])
+{
+    SDL_Init(0);
+    SDL_GetPrefPath("libsdl", "test_filesystem");
+    SDL_Quit();
+    return 0;
+}
