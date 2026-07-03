e6f44a8906b662b719f906f43a182b3b3fdd72f6
Ryan C. Gordon <icculus@icculus.org>
2026-07-03 00:56:28 -0400

    testvulkan: Move from a main function to the SDL main callbacks.
    This let me test it on HarmonyOS, since I currently _can't_ use a standard
    main() entry point there.
    
    (Vulkan actually works on the device!)

--- test/testvulkan.c
+++ test/testvulkan.c
@@ -12,9 +12,10 @@
 #include <stdlib.h>
 
 #include <SDL3/SDL_test_common.h>
-#include <SDL3/SDL_main.h>
 
 #if defined(SDL_PLATFORM_ANDROID) && defined(__ARM_EABI__) && !defined(__ARM_ARCH_7A__)
+
+#include <SDL3/SDL_main.h>
 
 int main(int argc, char *argv[])
 {
@@ -23,6 +24,9 @@ int main(int argc, char *argv[])
 }
 
 #else
+
+#define SDL_USE_MAIN_CALLBACKS 1
+#include <SDL3/SDL_main.h>
 
 #define VK_NO_PROTOTYPES
 #ifdef HAVE_VULKAN_H
@@ -1144,19 +1148,18 @@ static bool render(void)
     return true;
 }
 
-int main(int argc, char **argv)
+static Uint32 frames;
+static Uint64 then;
+
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
-    int done;
     const SDL_DisplayMode *mode;
-    SDL_Event event;
-    Uint64 then, now;
-    Uint32 frames;
     int dw, dh;
 
     /* Initialize test framework */
     state = SDLTest_CommonCreateState(argv, SDL_INIT_VIDEO);
     if (!state) {
-        return 1;
+        return SDL_APP_FAILURE;
     }
 
     /* Set Vulkan parameters */
@@ -1165,7 +1168,7 @@ int main(int argc, char **argv)
 
     if (!SDLTest_CommonDefaultArgs(state, argc, argv) || !SDLTest_CommonInit(state)) {
         SDLTest_CommonQuit(state);
-        return 1;
+        return SDL_APP_FAILURE;
     }
 
     mode = SDL_GetCurrentDisplayMode(SDL_GetPrimaryDisplay());
@@ -1183,40 +1186,52 @@ int main(int argc, char **argv)
     /* Main render loop */
     frames = 0;
     then = SDL_GetTicks();
-    done = 0;
-    while (!done) {
-        /* Check for events */
-        frames++;
-        while (SDL_PollEvent(&event)) {
-            /* Need to destroy the swapchain before the window created
-             * by SDL.
-             */
-            if (event.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED) {
-                destroySwapchainAndSwapchainSpecificStuff(true);
-            }
-            SDLTest_CommonEvent(state, &event, &done);
-        }
 
-        if (!done) {
-            int i;
-            for (i = 0; i < state->num_windows; ++i) {
-                if (state->windows[i]) {
-                    vulkanContext = &vulkanContexts[i];
-                    render();
-                }
-            }
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    int done = 0;
+
+    /* Need to destroy the swapchain before the window created
+     * by SDL.
+     */
+    if (event->type == SDL_EVENT_WINDOW_CLOSE_REQUESTED) {
+        destroySwapchainAndSwapchainSpecificStuff(true);
+    }
+
+    SDLTest_CommonEvent(state, event, &done);
+    return done ? SDL_APP_SUCCESS : SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    int i;
+
+    frames++;
+    for (i = 0; i < state->num_windows; ++i) {
+        if (state->windows[i]) {
+            vulkanContext = &vulkanContexts[i];
+            render();
         }
     }
 
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
     /* Print out some timing information */
-    now = SDL_GetTicks();
-    if (now > then) {
-        SDL_Log("%2.2f frames per second", ((double)frames * 1000) / (now - then));
+    if (result != SDL_APP_FAILURE) {
+        const Uint64 now = SDL_GetTicks();
+        if (now > then) {
+            SDL_Log("%2.2f frames per second", ((double)frames * 1000) / (now - then));
+        }
     }
 
     shutdownVulkan(true);
     SDLTest_CommonQuit(state);
-    return 0;
 }
 
 #endif
